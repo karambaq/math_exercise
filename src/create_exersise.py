c7ce09b20d8ebe71a4750ce2ebb25c0e87268f22
karambaq <0maximq@gmail.com>
2018-06-15 13:36:39 +0300

    fix

--- src/create_exersise.py
+++ src/create_exersise.py
@@ -10,10 +10,15 @@ def create_random_exercise():
     while True:
         exercise = ''
         for i in range(3):
-            digit = str(random.randint(1, 100))
+            digit = str(random.randint(10, 99))
             operation = random.choice([' + ', ' - '])
             exercise += digit + operation
-        digit = str(random.randint(1, 100))
+            if eval(exercise[:-2]) < 0 or eval(exercise[:-2]) > 100:
+                break
+
+        if eval(exercise[:-2]) not in range(0, 99):
+            continue
+        digit = str(random.randint(10, 99))
         exercise += digit
-        if eval(exercise) < 100:
+        if eval(exercise) in range(0, 99):
             return exercise + ' = ?'
