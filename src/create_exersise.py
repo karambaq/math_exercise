97a8124376725a9c7e42b7bee04b39333eb4cb41
karambaq <0maximq@gmail.com>
2018-07-02 15:10:07 +0300

    evals must be in (1, 99) range

--- src/create_exersise.py
+++ src/create_exersise.py
@@ -10,15 +10,15 @@ def create_random_exercise():
     while True:
         exercise = ''
         for i in range(3):
-            digit = str(random.randint(10, 99))
+            digit = str(random.choice([x for x in range(11, 99) if x % 10 != 0]))
             operation = random.choice([' + ', ' - '])
             exercise += digit + operation
             if eval(exercise[:-2]) < 0 or eval(exercise[:-2]) > 100:
                 break
 
-        if eval(exercise[:-2]) not in range(0, 99):
+        if eval(exercise[:-2]) not in range(1, 99):
             continue
-        digit = str(random.randint(10, 99))
+        digit = str(random.choice([x for x in range(11, 99) if x % 10 != 0]))
         exercise += digit
-        if eval(exercise) in range(0, 99):
+        if eval(exercise) in range(1, 99):
             return exercise + ' = ?'
